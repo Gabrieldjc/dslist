cf8a6879f0df87769e3f5cd870d6fc182b4d9227
Gabrieldjc <gabrieljc@protonmail.com>
2023-06-27 17:46:26 -0300

    get lists

--- dslist/src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ dslist/src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -1,11 +1,11 @@
 package com.devsuperior.dslist.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
 import com.devsuperior.dslist.entities.Game;
 
+@Service
 public interface GameRepository extends JpaRepository<Game, Long>{
 
 	
